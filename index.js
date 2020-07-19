848f6c749054ae7ee8f7f5913866197f7d4b7caa
Pascu <pascucosti9@gmail.com>
2020-07-19 15:09:39 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+require('dotenv').config();
+
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const APIAI_SESSION_ID = process.env.APIAI_SESSION_ID;
+
+const chalk = require('chalk');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(port, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log(chalk.green('a user connected'));
+});
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log(chalk.yellowBright('Message: ' + text));
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log(chalk.blueBright('Bot reply: ' + aiText));
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
